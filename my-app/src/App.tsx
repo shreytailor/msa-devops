d7f5d102438c0dfafaf0ddb182eb7cd7fcd0d1c5
Shrey Tailor <shreym.tailor@gmail.com>
2020-07-24 13:49:43 +1200

    testing api key in productioon

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 
 // Imports for the other components.
@@ -7,7 +7,26 @@ import MovieView from './components/MovieView/MovieView';
 // Imports for Material UI.
 import TextField from '@material-ui/core/TextField';
 
+interface MovieState {
+  posterURL: string,
+  title: string,
+  year: string,
+  plot: string,
+  runtime: string,
+  director: string,
+  imdbRating: string
+}
+
+interface OtherState {
+  exists: Boolean,
+  error: Boolean
+}
+
 function App() {
+  // Configuring the state of the main component.
+  const [movie, setMovie] = useState<MovieState>();
+  const [otherState, setOtherState] = useState<OtherState>({exists: false, error: false})
+
   return (
     <div className="App">
       {/* Using Material UI to create the textfield for entering the movie ID.*/}
@@ -24,26 +43,46 @@ function App() {
         onKeyDown={
           function(event) {
             if (event.keyCode === 13) {
-              fetch(`http://www.omdbapi.com/?apikey=${process.env.REACT_APP_API_KEY}&i=tt2293640&plot=full`)
+              const target = event.target as HTMLTextAreaElement;
+              fetch(`http://www.omdbapi.com/?apikey=${process.env.REACT_APP_API_KEY}&i=${target.value}&plot=full`)
               .then(response => response.json())
-              .then(response => console.log(response))
-              .catch(() => console.log("There was an error regarding the API being used. ):"));
+              .then(response => {
+                if (response["Error"]) {
+                  alert("Error! Make sure the imDb ID was entered correctly. If error persists, contact the developer ):")
+                } else {
+                  setMovie({
+                    posterURL: response["Poster"],
+                    title:  response["Title"],
+                    year:  response["Year"],
+                    runtime:  response["Runtime"],
+                    director:  response["Director"],
+                    plot:  response["Plot"],
+                    imdbRating:  response["imdbRating"]
+                  })
+                  setOtherState({
+                    exists: true,
+                    error: false
+                  })
+                }
+              })
+              .catch(() => alert("There was an error regarding the API being used. ):"));
             }
           }
         }
       />
 
       {/* A custom functional component showing all the details about the movie.*/}
-      <MovieView 
-        posterURL="https://m.media-amazon.com/images/M/MV5BMTg2MTMyMzU0M15BMl5BanBnXkFtZTgwOTU3ODk4NTE@._V1_SX300.jpg"
-        title="Minions"
-        year="2015"
-        runtime="91 min"
-        director="Kyle Balda, Pierre Coffinv"
-        plot="Ever since the dawn of time, the Minions have lived to serve the most despicable of masters. From the T-Rex to Napoleon, the easily distracted tribe has helped the biggest and the baddest of villains. Now, join protective leader Kevin, teenage rebel Stuart, and lovable little Bob on a global road trip. They'll earn a shot to work for a new boss, the world's first female supervillain, and try to save all of Minionkind from annihilation."
-        imdbRating="6.4"  
-      />
-
+      {otherState["exists"]
+      ? <MovieView 
+        posterURL={movie?.posterURL!}
+        title={movie?.title!}
+        year={movie?.year!}
+        runtime={movie?.runtime!}
+        director={movie?.director!}
+        plot={movie?.plot!}
+        imdbRating={movie?.imdbRating!}
+        />
+      : null}
     </div>
   );
 }
